e144f472bb3c7b4e84fd71fd22a7263eca355f49
mohamedrashad102 <mmm682552@gmail.com>
2024-05-26 03:12:00 +0300

    add light theme

---
+++ lib/core/utils/light_theme.dart
@@ -0,0 +1,7 @@
+import 'package:bank_dash/core/utils/coloors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData.light().copyWith(
+  
+  scaffoldBackgroundColor: Coloors.scaffoldBgColor,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bank_dash/core/utils/app_router.dart';
+import 'package:bank_dash/core/utils/light_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
+      theme: lightTheme,
     );
   }
 }
