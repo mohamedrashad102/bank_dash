e4d96aed5fedf905c5c9053dfa0265d44a01de59
mohamedrashad102 <mmm682552@gmail.com>
2024-06-22 03:44:01 +0300

    fixed credit card and finish mobile over view (add transfer section)

--- lib/layers/mobile_layout/overview/widgets/balance_history.dart
+++ lib/layers/mobile_layout/overview/widgets/balance_history.dart
@@ -1,6 +1,9 @@
 import 'package:bank_dash/core/widgets/title_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
+
+import 'balance_history_chart.dart';
 
 class BalanceHistory extends StatelessWidget {
   const BalanceHistory({super.key});
@@ -11,7 +14,12 @@ class BalanceHistory extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         TitleText(title: 'Balance History'),
-        Gap(12),
+        Gap(30),
+        SizedBox(
+          height: 230,
+          width: double.infinity,
+          child: BalanceHistoryChart(),
+        ),
       ],
     );
   }
--- lib/layers/mobile_layout/overview/widgets/quick_transfer.dart
+++ lib/layers/mobile_layout/overview/widgets/quick_transfer.dart
@@ -1,13 +1,11 @@
-import 'package:bank_dash/core/utils/app_colors.dart';
-import 'package:bank_dash/core/utils/app_styles/app_styles.dart';
 import 'package:bank_dash/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../core/widgets/title_text.dart';
 import '../models/transfer_model.dart';
-import 'transfer_items.dart';
+import 'quick_transfer_bottom_section.dart';
+import 'quick_transfer_peoples.dart';
 
 class QuickTransfer extends StatelessWidget {
   const QuickTransfer({super.key});
@@ -32,88 +30,16 @@ class QuickTransfer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Gap(12),
-        const TitleText(title: 'Quick Transfer'),
-        const Gap(12),
-        Row(
-          children: [
-            const TransferItems(),
-            const Gap(12),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                padding: const EdgeInsets.all(10),
-                child: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: Color(0xff718EBF),
-                  size: 30,
-                ),
-              ),
-            ),
-          ],
-        ),
-        const Gap(12),
-        Row(
-          children: [
-            Text(
-              'Write Amount',
-              style: AppStyles.regular.fontSize12(context),
-            ),
-            const Gap(40),
-            Stack(
-              alignment: Alignment.centerRight,
-              children: [
-                Container(
-                  height: 40,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: const Color(0xffEDF1F7),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text(
-                    '525.50',
-                    style: AppStyles.regular.fontSize12(context),
-                  ),
-                ),
-                Container(
-                  height: 40,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: AppColors.contentColorBlue,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  alignment: Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Send',
-                        style: AppStyles.medium.fontSize13(context).copyWith(
-                              color: Colors.white,
-                            ),
-                      ),
-                      const Gap(10),
-                      SvgPicture.asset(
-                        Assets.imagesSendIcon,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        const Gap(12),
+        Gap(12),
+        TitleText(title: 'Quick Transfer'),
+        Gap(12),
+        QuickTransferPeoples(),
+        Gap(12),
+        QuickTransferBottomSection(),
+        Gap(12),
       ],
     );
   }
